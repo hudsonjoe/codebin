1fed7cb5d555869f9a7003db00d96b4308098629
hudsonjoe <61133036+hudsonjoe@users.noreply.github.com>
2020-02-17 12:49:02 +0530

    Add files via upload
    guvi day 02

---
+++ repeating a tast.py
@@ -0,0 +1,13 @@
+r=1
+print("welcome")
+print("you got to choose a option")
+while(r!=0):
+    b=input("1.college,2.vacation,q.to quit")
+    if (b!='q'):
+        r=1
+    else:
+        print("end")
+        break
+
+    
+       
